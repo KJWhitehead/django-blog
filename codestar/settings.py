fdaf47c37e93ec927c3b9e1367be47e38ed0dac0
KJ Whitehead <kenyon.whitehead@gmail.com>
2024-03-01 13:34:38 +0000

    Feat: delete comment

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-kjwhitehead-djangoblog-93f1cqdhh0j.ws-eu108.gitpod.io',
                 '.herokuapp.com']
